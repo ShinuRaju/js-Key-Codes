a4707a5546880fd1b7000c27a37119db3a3ac5c1
Shinu <rajushinu007@gmail.com>
2021-04-30 19:10:31 +0530

    js added

---
+++ app.js
@@ -0,0 +1,29 @@
+// let container = document.querySelector(".container");
+
+// window.addEventListener("keydown", (e) => {
+//   container.innerHTML = `
+// <div class="content-box">
+// <h1>${e.key == " " ? "space" : e.key}</h1>
+//     <div class="four-box">
+//         <div class="eventkey"e>
+//             <small>event.key</small>
+//             <h3>${e.key == " " ? "space" : e.key}</h3>
+//         </div>
+//         <div class="eventloc">
+//         <small>e.which</small>
+//         <h3>${e.which}</h3>
+//         </div>
+//         <div class="eventwhi">
+
+//             <small>event.location</small>
+//             <h3>${e.code}</h3>
+//         </div>
+//         <div class="eventcod">
+//             <small>key code </small>
+//             <h3>${e.keyCode}</h3>
+//         </div>
+//     </div>
+// </div>
+
+// `;
+// });
